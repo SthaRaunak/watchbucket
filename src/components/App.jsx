53a9958d5f4f9d58ae2f6a247dbb87a35eb88c4a
Raunak Shrestha <raunaknagendra@gmail.com>
2023-10-20 10:02:20 +0545

    added logic to fetch movies from api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dashboard from "../pages/Dashboard";
 import Home from "../pages/Home";
 import Navbar from "./Navbar";
 import SearchedMovieList from "./SearchedMovieList";
 import Search from "./Search";
 
+const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+
 function App() {
+  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("");
-  console.log(query);
+
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${apiKey}&${
+            query ? "s=" + query : "s=movies"
+          }`
+        );
+        const data = await res.json();
+        console.log(data);
+        setMovies(data.Search);
+      } catch (error) {
+        console.error(error.message);
+      }
+    }
+
+    fetchMovies();
+  }, [query]);
+
   return (
     <>
       <BrowserRouter>
