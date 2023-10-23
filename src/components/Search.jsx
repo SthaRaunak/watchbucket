8b7c1eb60428922ac9b11e4a92c7ee834b2a0fc3
Raunak Shrestha <raunaknagendra@gmail.com>
2023-10-23 23:35:15 +0545

    Improved search bar: added dynamic paste and clear button with functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,32 @@
 function Search({ query, setQuery }) {
+  const handlePaste = async () => {
+    if (!query) {
+      const text = await navigator.clipboard.readText();
+      setQuery(text);
+    } else if (query) {
+      setQuery("");
+    }
+  };
+
   return (
     <>
       <section className="bg-[#0c0f11] h-full border-b-gray-500 border-b-[1px]">
-        <div className="max-w-[1200px] mx-auto px-5">
+        <div className="max-w-[1200px] mx-auto px-5 flex items-center gap-14">
           <input
             type="text"
-            className="text-5xl py-8 outline-none  bg-transparent text-white font-extralight w-full"
+            className={`text-5xl ${
+              !query ? "py-9" : "py-5"
+            } outline-none  bg-transparent text-white font-extralight w-[85%]`}
             placeholder="Type to find movies, TV show, anime"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
           />
+          <button
+            className="text-lg text-[#9DA2AF] h-full"
+            onClick={handlePaste}
+          >
+            {query ? "CLEAR" : "PASTE"}
+          </button>
         </div>
       </section>
     </>
