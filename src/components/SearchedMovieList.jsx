283acda06d6a8b7478c381d4ab2059494f1e60a2
Raunak Shrestha <raunaknagendra@gmail.com>
2023-10-27 23:06:30 +0545

    Added sort by section UI in searched movie list

--- src/components/SearchedMovieList.jsx
+++ src/components/SearchedMovieList.jsx
@@ -2,8 +2,20 @@ import { Link } from "react-router-dom";
 
 function SearchedMovieList({ movies }) {
   return (
-    <div className="max-w-[1200px] mx-auto px-5 pt-9">
-      <div className="flex w-full mt-5 border-b-[1px] border-gray-600">
+    <div className="max-w-[1200px] mx-auto px-5 pt-5">
+      <div className="flex gap-5 items-center">
+        <p className="text-gray-400 font-medium">Sort By: </p>
+        <button className="bg-[#131617] px-4 py-1 rounded-xl hover:bg-[#35383A] ease-linear duration-75">
+          Relevance
+        </button>
+        <button className="bg-[#131617] px-4 py-1 rounded-xl hover:bg-[#35383A] ease-linear duration-75">
+          Release date
+        </button>
+        <button className="bg-[#131617] px-4 py-1 rounded-xl hover:bg-[#35383A] ease-linear duration-75">
+          By Title
+        </button>
+      </div>
+      <div className="flex w-full mt-8 border-b-[1px] border-gray-600">
         <h5 className="w-[15%] ps-5">Year</h5>
         <h5 className="w-[75%]">Title</h5>
         <h5 className="w-[10%]">Type</h5>
@@ -24,7 +36,7 @@ function Movie({ movie }) {
       <img
         src={movie.Poster}
         alt=""
-        className="absolute top-0 w-[150px] h-[232px] right-[1161px] invisible group-hover:visible opacity-70"
+        className="absolute top-0 w-[150px] h-[232px] right-[1160px] invisible group-hover:visible opacity-95"
       />
       <p className="w-[15%] text-gray-400 text-sm ps-5">{movie.Year}</p>
       <div className="w-[75%] flex gap-5 items-center">
